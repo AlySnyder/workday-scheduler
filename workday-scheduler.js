a439376d2a60ebb8c20873ddd555f4c603544bae
Alex Snyder <alystrife@gmail.com>
2021-10-16 10:02:57 -0400

    added past present future function

--- workday-scheduler.js
+++ workday-scheduler.js
@@ -11,4 +11,29 @@ function saveHandler() {
 
 $('.saveBtn').on('click', saveHandler);
 
+function checkHourPassed() {
+    // get current number of hours, get this hour 
+    var thisHour = moment().hours();
+
+    // loop over time blocks
+    $('.time-block').each(function () {
+      var scheduledHour = parseInt($(this).attr('id'));
+
+      // check if we've moved past this time
+      if (scheduledHour < thisHour) {
+        $(this).addClass('past');
+      } else if (scheduledHour === thisHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+
+  checkHourPassed();
+
+
 });
